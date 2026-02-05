34bc0f63492f6f0f2ad7ddb7b7c0697b5a5d329d
Jonathan Amsterdam <jba@google.com>
2026-02-05 12:55:18 -0500

    params table

--- db.sql
+++ db.sql
@@ -2,10 +2,15 @@ CREATE TABLE modules (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     path TEXT NOT NULL,
     state TEXT NOT NULL
-);
+) STRICT;
 
 CREATE TABLE packages (
     module_id INTEGER NOT NULL,
     relative_path TEXT NOT NULL,
     FOREIGN KEY (module_id) REFERENCES modules(id)
-);
+) STRICT;
+
+CREATE TABLE params (
+    name TEXT NOT NULL,
+    value TEXT NOT NULL
+) STRICT;
